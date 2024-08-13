bdfcad37d7a9a1c6b97e50aed2089b5107c5b80e
ArisaTaki <570490640@qq.com>
2024-08-13 10:32:21 +0800

    修改项目结构

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Home: React.FC = () => {
+  return <div className={styles.home}>Home</div>;
+};
+
+export default Home;
