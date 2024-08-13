a4f741541b7a7a37b2e0cadba8d222f48e998b01
ArisaTaki <570490640@qq.com>
2024-08-13 20:59:28 +0800

    添加加载动画

--- src/Experience/World/World.ts
+++ src/Experience/World/World.ts
@@ -4,20 +4,19 @@ import TestObject from "../Objects/TestObject/TestObject";
 import ObjectEnum from "../ObjectEnum";
 import BaseObject from "../Objects/BaseObject/BaseObject";
 import Experience from "../Experience";
+import HomeStyles from "@/views/Home/styles.module.css";
 export default class World extends kokomi.Component {
   declare base: Experience;
   testObject?: TestObject;
   baseObject?: BaseObject;
 
   renderTestObject = () => {
-    this.base.am.on("ready", () => {
-      const skyBox = this.base.am.items["skyBox"];
-      skyBox.mapping = THREE.EquirectangularReflectionMapping;
-      this.base.scene.background = skyBox;
+    const skyBox = this.base.am.items["skyBox"];
+    skyBox.mapping = THREE.EquirectangularReflectionMapping;
+    this.base.scene.background = skyBox;
 
-      this.testObject = new TestObject(this.base);
-      this.testObject.addExisting();
-    });
+    this.testObject = new TestObject(this.base);
+    this.testObject.addExisting();
   };
 
   renderBaseObject = () => {
@@ -27,13 +26,21 @@ export default class World extends kokomi.Component {
 
   constructor(base: Experience, objectEnum?: ObjectEnum) {
     super(base);
-    switch (objectEnum) {
-      case ObjectEnum.TestObject:
-        this.renderTestObject();
-        break;
-      case ObjectEnum.BaseObject:
-        this.renderBaseObject();
-        break;
-    }
+
+    this.base.am.on("ready", () => {
+      setTimeout(() => {
+        document
+          .querySelector(`.${HomeStyles["loader-screen"]}`)
+          ?.classList.add(HomeStyles["hollow"]);
+        switch (objectEnum) {
+          case ObjectEnum.TestObject:
+            this.renderTestObject();
+            break;
+          case ObjectEnum.BaseObject:
+            this.renderBaseObject();
+            break;
+        }
+      }, 3000);
+    });
   }
 }
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -6,9 +6,22 @@ import ObjectEnum from "@/Experience/ObjectEnum";
 const Home: React.FC = () => {
   useApplyShader({ id: "#home", objectEnum: ObjectEnum.TestObject });
   return (
-    <div id="home" className={styles["home"]}>
-      Home
-    </div>
+    <>
+      <div id="home" className={styles["home"]} />
+      <div className={styles["loader-screen"]}>
+        <div className={styles["loading-container"]}>
+          <div className={styles["loading"]}>
+            <span style={{ "--i": 0 } as React.CSSProperties}>L</span>
+            <span style={{ "--i": 1 } as React.CSSProperties}>O</span>
+            <span style={{ "--i": 2 } as React.CSSProperties}>A</span>
+            <span style={{ "--i": 3 } as React.CSSProperties}>D</span>
+            <span style={{ "--i": 4 } as React.CSSProperties}>I</span>
+            <span style={{ "--i": 5 } as React.CSSProperties}>N</span>
+            <span style={{ "--i": 6 } as React.CSSProperties}>G</span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
