244c649a7f81bd4251b29a76bea869e11804d3d7
ArisaTaki <570490640@qq.com>
2024-08-15 15:07:11 +0800

    使用动态导入

--- src/Experience/World/index.ts
+++ src/Experience/World/index.ts
@@ -2,8 +2,7 @@ import * as kokomi from "kokomi.js";
 import ObjectEnum from "../ObjectEnum";
 import Experience from "../Experience";
 import LoadingStyles from "@/components/loadingComp/styles.module.css";
-import BaseWorld from "./BaseWorld";
-import TestWorld from "./TestWorld";
+
 export default class World extends kokomi.Component {
   // 将base定义为Experience类
   declare base: Experience;
@@ -13,18 +12,23 @@ export default class World extends kokomi.Component {
 
     // 加载好材质之后进行的行为
     this.base.am.on("ready", () => {
-      setTimeout(() => {
+      setTimeout(async () => {
         document
           .querySelector(`.${LoadingStyles["loader-screen"]}`)
           ?.classList.add(LoadingStyles["hollow"]);
-        // 根据objectEnum决定渲染哪一个shader Object
+
+        // 根据objectEnum决定动态导入并渲染对应的shader Object
         switch (objectEnum) {
-          case ObjectEnum.TestObject:
+          case ObjectEnum.TestObject: {
+            const { default: TestWorld } = await import("./TestWorld");
             new TestWorld(this.base);
             break;
-          case ObjectEnum.BaseObject:
+          }
+          case ObjectEnum.BaseObject: {
+            const { default: BaseWorld } = await import("./BaseWorld");
             new BaseWorld(this.base);
             break;
+          }
         }
       }, 2000);
     });
