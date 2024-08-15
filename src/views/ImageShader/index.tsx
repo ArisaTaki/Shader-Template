a1f8d888e290341a6dfcea76fe00bd51ea7ec34a
ArisaTaki <570490640@qq.com>
2024-08-15 15:44:08 +0800

    设置图片案例的基本框架dom

--- src/views/ImageShader/index.tsx
+++ src/views/ImageShader/index.tsx
@@ -1,8 +1,51 @@
-const ImageShader = () => {
+import React from "react";
+import styles from "./styles.module.css";
+import useApplyShader from "@/hooks/useApplyShader";
+import ObjectEnum from "@/Experience/ObjectEnum";
+import LoadingComp from "@/components/loadingComp";
+const ImageShader: React.FC = () => {
+  useApplyShader({
+    id: "#image",
+    objectEnum: ObjectEnum.ImageObject,
+    useKokomiOrbitControls: true,
+  });
   return (
-    <div>
-      <h1>ImageShader</h1>
-    </div>
+    <>
+      <LoadingComp />
+      <div id="image" className={styles["image"]} />;
+      <div className={styles["gallery"]}>
+        <img
+          className={styles["gallery-item"]}
+          src="https://s2.loli.net/2023/09/12/ySLGYKhVqH3BtN4.jpg"
+          crossOrigin="anonymous"
+          alt=""
+        />
+        <img
+          className={styles["gallery-item"]}
+          src="https://s2.loli.net/2023/09/12/BhmSdM2XA9yYftK.jpg"
+          crossOrigin="anonymous"
+          alt=""
+        />
+        <img
+          className={styles["gallery-item"]}
+          src="https://s2.loli.net/2023/09/12/CqIlJd1XO9rh68e.jpg"
+          crossOrigin="anonymous"
+          alt=""
+        />
+        <img
+          className={styles["gallery-item"]}
+          src="https://s2.loli.net/2023/09/12/RzwqhImAV9H57xs.jpg"
+          crossOrigin="anonymous"
+          alt=""
+        />
+        <img
+          className={styles["gallery-item"]}
+          src="https://s2.loli.net/2023/09/12/p3FME9qcUAnJixm.jpg"
+          crossOrigin="anonymous"
+          alt=""
+        />
+      </div>
+    </>
   );
 };
 
