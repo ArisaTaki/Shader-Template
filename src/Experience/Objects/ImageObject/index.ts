07dad27fd5ec89cf5235fe1d67dc5846075722e7
ArisaTaki <570490640@qq.com>
2024-08-15 15:43:28 +0800

    使用slider类

--- src/Experience/Objects/ImageObject/index.ts
+++ src/Experience/Objects/ImageObject/index.ts
@@ -1,58 +1,24 @@
-import * as THREE from "three";
-import * as kokomi from "kokomi.js";
-import vertexShader from "./Shaders/vert.glsl";
-import fragmentShader from "./Shaders/frag.glsl";
 import Experience from "@/Experience/Experience";
+import * as kokomi from "kokomi.js";
+import ImageStyles from "@/views/ImageShader/styles.module.css";
 
 export default class ImageObject extends kokomi.Component {
   declare base: Experience;
-  material: THREE.ShaderMaterial;
-  mesh: THREE.Mesh<
-    THREE.SphereGeometry,
-    THREE.ShaderMaterial,
-    THREE.Object3DEventMap
-  >;
-  uj: kokomi.UniformInjector;
+  ig: kokomi.InfiniteGallery;
   constructor(base: Experience) {
     super(base);
 
-    const params = {
-      uDistort: {
-        value: 1,
-      },
-    };
-
-    const geometry = new THREE.SphereGeometry(2, 64, 64);
-    const material = new THREE.ShaderMaterial({
-      vertexShader: vertexShader,
-      fragmentShader: fragmentShader,
+    this.ig = new kokomi.InfiniteGallery(this.base, {
+      elList: [
+        ...document.querySelectorAll(`.${ImageStyles["gallery-item"]}`),
+      ] as HTMLImageElement[],
+      direction: "horizontal",
+      gap: 128,
     });
-    this.material = material;
-    const mesh = new THREE.Mesh(geometry, material);
-    this.mesh = mesh;
+  }
 
-    const uj = new kokomi.UniformInjector(this.base);
-    this.uj = uj;
-    material.uniforms = {
-      ...material.uniforms,
-      ...uj.shadertoyUniforms,
-      ...params,
-    };
-    const debug = this.base.debug;
-    if (debug.active) {
-      const debugFolder = debug.ui?.addFolder("imageObject");
-      debugFolder
-        ?.add(params.uDistort, "value")
-        .min(0)
-        .max(2)
-        .step(0.01)
-        .name("distort");
-    }
-  }
-  addExisting() {
-    this.container.add(this.mesh);
-  }
-  update() {
-    this.uj.injectShadertoyUniforms(this.material.uniforms);
+  async addExisting() {
+    this.ig.addExisting();
+    await this.ig.checkImagesLoaded();
   }
 }
