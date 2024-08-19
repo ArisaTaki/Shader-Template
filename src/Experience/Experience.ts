cbb3d32d4b6e2aec0df579e0dfe935bb85884fab
ArisaTaki <570490640@qq.com>
2024-08-19 21:45:27 +0800

    订正参数

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -10,7 +10,7 @@ export interface ExperienceConfig {
   // 传递给 World 的配置
   objectEnum: ObjectEnum;
   // 先设定一个设置摄像机位置的参数
-  cameraPosition?: THREE.Vector3;
+  cameraPosition?: { x: number; y: number; z: number };
 }
 
 export default class Experience extends kokomi.Base {
@@ -22,7 +22,13 @@ export default class Experience extends kokomi.Base {
     super(config.id);
 
     // 动态设置摄像机位置
-    const cameraPosition = config?.cameraPosition || new THREE.Vector3(0, 0, 5);
+    const cameraPosition = config.cameraPosition
+      ? new THREE.Vector3(
+          config.cameraPosition.x,
+          config.cameraPosition.y,
+          config.cameraPosition.z
+        )
+      : new THREE.Vector3(0, 0, 5);
     // 把摄像机的位置设置给camera
     this.camera.position.copy(cameraPosition);
     // 添加轨道控制
