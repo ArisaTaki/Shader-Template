8fbaf777da4b89bc116dcc74366083383ea644eb
ArisaTaki <570490640@qq.com>
2024-08-22 09:24:07 +0800

    尝试了一些别的噪音

--- src/Experience/Objects/ThreedObject/Shaders/vert.glsl
+++ src/Experience/Objects/ThreedObject/Shaders/vert.glsl
@@ -1,4 +1,8 @@
 #include "/node_modules/lygia/generative/cnoise.glsl"
+#include "/node_modules/lygia/generative/snoise.glsl"
+#include "/node_modules/lygia/generative/pnoise.glsl"
+#include "/node_modules/lygia/generative/fbm.glsl"
+#include "/node_modules/lygia/generative/curl.glsl"
 #include "/node_modules/lygia/math/const.glsl"
 
 uniform float iTime;
@@ -11,7 +15,22 @@ varying float vNoise;
 varying vec3 vNormal;
 varying vec3 vWorldPosition;
 vec3 distort(vec3 p){
-    float offset=cnoise(p / uFrequency + iTime * 0.3);
+    // simplex noise
+    float offset=snoise(p / uFrequency + iTime * 0.3);
+    
+    // classic perlin noise
+    // float offset=cnoise(p / uFrequency + iTime * 0.3);
+
+    // periodic noise
+    // vec3 repetition = vec3(10.0, 10.0, 10.0); // 设置pnoise噪声的重复周期
+    // float offset=pnoise(p / uFrequency + iTime * 0.3,repetition);
+
+    // fbm noise
+    // float offset=fbm(p / uFrequency + iTime * 0.3);
+
+    // curl noise
+    // float offset=curl(p / uFrequency + iTime * 0.3).y;
+
     float t=(p.y+offset)*PI*12.;
     float noise=(sin(t)*p.x+cos(t)*p.z)*2.;
     noise*=uDistort;
